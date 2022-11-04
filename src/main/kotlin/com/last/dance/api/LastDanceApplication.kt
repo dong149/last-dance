6c7e4d8c4c8ba43aa6c8a2e83dbe3bb3ed01fd58
Patrick Ryoo <donghoon149@gmail.com>
2022-11-05 03:51:22 +0900

    initial setting

---
+++ src/main/kotlin/com/last/dance/api/LastDanceApplication.kt
@@ -0,0 +1,11 @@
+package com.last.dance.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LastDanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<LastDanceApplication>(*args)
+}
---
+++ src/test/kotlin/com/last/dance/api/LastDanceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.last.dance.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LastDanceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
